751780d1f7f9b31dbee211b6dcfb5db1067f010d
Shubham Gopale <130681179+shubh1928@users.noreply.github.com>
2025-06-10 20:44:59 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { TaskComponent } from './task/task.component';
 
-// export const routes: Routes = [
-//     {path: '', component: TaskComponent}
-// ];
-
-
 export const routes: Routes = [];
-
 
 //Routes are used to define the navigation paths in the application.
